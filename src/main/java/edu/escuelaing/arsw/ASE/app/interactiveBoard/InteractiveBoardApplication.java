9304d1c611658595613c8a92fd9b9a963eeae780
Juanes <juaneortiz1@gmail.com>
2024-06-27 12:46:52 -0500

    First commit

---
+++ src/main/java/edu/escuelaing/arsw/ASE/app/interactiveBoard/InteractiveBoardApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.ASE.app.interactiveBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/arsw/ASE/app/interactiveBoard/InteractiveBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.ASE.app.interactiveBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
